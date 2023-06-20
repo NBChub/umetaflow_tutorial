49d180c2e68669da2857a57c0784a91447f20350
Efi Kontou <54527247+eeko-kon@users.noreply.github.com>
2023-06-20 21:51:23 +0200

    Update setup.sh
    resources dir sirius

--- setup.sh
+++ setup.sh
@@ -11,9 +11,9 @@ function display_help {
     echo "Download resources required for running umetaflow."
     echo ""
     echo "Arguments:"
-    echo "  folder  : (Optional) The folder where the files will be downloaded. Default: $default_folder"
-    echo "  ion_mode  : (Optional) The ionization mode of the data ("positive" or "negative"). Default: $default_mode"
-    echo "  MY_OS  : (Optional) The operating system that is used ("osx64" for macOS and "linux64" for linux). Default: $default_OS"
+    echo "folder:         (Optional) The folder where the files will be downloaded. Default: $default_folder"
+    echo "ion_mode:       (Optional) The ionization mode of the data ("positive" or "negative"). Default: $default_mode"
+    echo "MY_OS:          (Optional) The operating system that is used ("osx64" for macOS and "linux64" for linux). Default: $default_OS"
     echo ""
 }
 
@@ -33,9 +33,9 @@ ion_mode=${1:-$default_mode}
 MY_OS=${1:-$default_OS}
 
 # get sirius executable
-curl -s https://api.github.com/repos/boecker-lab/sirius/releases/latest | tr -d '"' | grep "browser_download_url.*${MY_OS}.zip$"| cut -d : -f 2,3 |  wget -i- 
+(cd resources && curl -s https://api.github.com/repos/boecker-lab/sirius/releases/latest | tr -d '"' | grep "browser_download_url.*${MY_OS}.zip$"| cut -d : -f 2,3 |  wget -i- && unzip *.zip)
 
 # Get the directory of the current script
 script_directory=$(dirname "$0")
 echo $script_directory
-bash $script_directory/download_models.sh $script_directory/zenodo_{default_mode}.txt
+bash $script_directory/download_models.sh $script_directory/zenodo_${default_mode}.txt
