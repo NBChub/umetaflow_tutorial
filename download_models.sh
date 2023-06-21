940999139b7d78d4e7e6120a17e61aeca15263c8
Efi Kontou <54527247+eeko-kon@users.noreply.github.com>
2023-06-21 11:14:17 +0200

    Update download_models.sh
    ms2query (correction from msquery)

--- download_models.sh
+++ download_models.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Specify the default folder where the files will be downloaded
-default_folder="resources/msquery"
+default_folder="resources/ms2query"
 
 # Help function
 function display_help {
