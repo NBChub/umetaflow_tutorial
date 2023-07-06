29ac2e3c203f0391dd364540c06c8e2a5ffd6841
eeko-kon <eeko@biosustain.dtu.dk>
2023-07-06 10:57:44 +0200

    fix arguments

--- download_models.sh
+++ download_models.sh
@@ -5,13 +5,13 @@ default_folder="resources/ms2query"
 
 # Help function
 function display_help {
-    echo "Usage: script.sh [folder]"
+    echo "Usage: script.sh [options]"
     echo "Download resources required for running umetaflow."
     echo ""
-    echo "Arguments:"
-    echo "folder:         (Optional) The folder where the files will be downloaded. Default: $default_folder"
-    echo "ion_mode:       (Optional) The ionization mode of the data ("positive" or "negative"). Default: $default_mode"
-    echo "MY_OS:          (Optional) The operating system that is used ("osx64" for macOS and "linux64" for linux). Default: $default_OS"
+    echo "Options:"
+    echo "-f, --folder <folder>       (Optional) The folder where the files will be downloaded. Default: $default_folder"
+    echo "-m, --ion_mode <ion_mode>   (Optional) The ionization mode of the data ('positive' or 'negative'). Default: $default_mode"
+    echo "-o, --MY_OS <MY_OS>         (Optional) The operating system that is used ('osx64' for macOS and 'linux64' for Linux). Default: $default_MY_OS"
     echo ""
 }
 
--- setup.sh
+++ setup.sh
@@ -7,38 +7,49 @@ default_OS="linux64"
 
 # Help function
 function display_help {
-    echo "Usage: script.sh [folder]"
+    echo "Usage: script.sh [options]"
     echo "Download resources required for running umetaflow."
     echo ""
-    echo "Arguments:"
-    echo "folder:         (Optional) The folder where the files will be downloaded. Default: $default_folder"
-    echo "ion_mode:       (Optional) The ionization mode of the data ("positive" or "negative"). Default: $default_mode"
-    echo "MY_OS:          (Optional) The operating system that is used ("osx64" for macOS and "linux64" for linux). Default: $default_OS"
+    echo "Options:"
+    echo "-f, --folder <folder>       (Optional) The folder where the files will be downloaded. Default: $default_folder"
+    echo "-m, --ion_mode <ion_mode>   (Optional) The ionization mode of the data ('positive' or 'negative'). Default: $default_mode"
+    echo "-o, --MY_OS <MY_OS>         (Optional) The operating system that is used ('osx64' for macOS and 'linux64' for Linux). Default: $default_MY_OS"
     echo ""
 }
 
-# Check if help option is provided
-if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-    display_help
-    exit 0
-fi
+# Default option values
+folder=$default_folder
+ion_mode=$default_mode
+MY_OS=$default_MY_OS
 
-# Specify the folder where the files will be downloaded
-folder=${1:-$default_folder}
+# Parse command-line options
+while getopts ":f:m:o:-:" opt; do
+    case $opt in
+        f) folder=$OPTARG ;;
+        m) ion_mode=$OPTARG ;;
+        o) MY_OS=$OPTARG ;;
+        -)
+            case $OPTARG in
+                folder) folder="${!OPTIND}" ;;
+                ion_mode) ion_mode="${!OPTIND}" ;;
+                MY_OS) MY_OS="${!OPTIND}" ;;
+                help) display_help; exit 0 ;;
+                *) echo "Invalid option: --$OPTARG"; exit 1 ;;
+            esac
+            OPTIND=$((OPTIND + 1))
+            ;;
+        \?) echo "Invalid option: -$OPTARG"; exit 1 ;;
+        :) echo "Option -$OPTARG requires an argument."; exit 1 ;;
+    esac
+done
 
-# Specify the ion mode
-ion_mode=${1:-$default_mode}
+# Get the SIRIUS executable
+(cd "$folder" && curl -s "https://api.github.com/repos/boecker-lab/sirius/releases/latest" | tr -d '"' | grep "browser_download_url.*${MY_OS}.zip$" | cut -d : -f 2,3 | wget -i- && unzip *.zip)
 
-# Specify the operating system
-MY_OS=${1:-$default_OS}
-
-# get the SIRIUS executable
-(cd ${default_folder} && curl -s https://api.github.com/repos/boecker-lab/sirius/releases/latest | tr -d '"' | grep "browser_download_url.*${MY_OS}.zip$"| cut -d : -f 2,3 |  wget -i- && unzip *.zip)
-
-# get the ThermoRawFileParser executable
-(mkdir ${default_folder}/ThermoRawFileParser && cd ${default_folder}/ThermoRawFileParser && wget https://github.com/compomics/ThermoRawFileParser/releases/download/v1.3.4/ThermoRawFileParser.zip && unzip ThermoRawFileParser.zip)
+# Get the ThermoRawFileParser executable
+(mkdir -p "$folder/ThermoRawFileParser" && cd "$folder/ThermoRawFileParser" && wget "https://github.com/compomics/ThermoRawFileParser/releases/download/v1.3.4/ThermoRawFileParser.zip" && unzip ThermoRawFileParser.zip)
 
 # Get the directory of the current script
 script_directory=$(dirname "$0")
-echo $script_directory
-bash $script_directory/download_models.sh $script_directory/zenodo_${default_mode}.txt
+echo "$script_directory"
+bash "$script_directory/download_models.sh" "$script_directory/zenodo_${ion_mode}.txt"
